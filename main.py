a8143125f48cfeaafaed46f6229746db28fbd133
Eric Cai <110260832+ericmcai@users.noreply.github.com>
2026-03-30 08:54:53 -0700

    Eric/pipeline (#1)
    * importing functions
    
    * create functions to retrieve pitcher data
    
    * edit timeframe

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import requests
+import pandas as pd
+import numpy as np
+from sklearn.mixture import GaussianMixture as GMM
+from pybaseball import statcast, statcast_pitcher, playerid_lookup, pitching_stats
 
+def get_player_id(first_name: str, last_name: str) -> int:
+    '''Get the MLBAM player ID for a given player name. Returns the player ID as an int.'''
+    player = playerid_lookup(last_name, first_name)
+    if not player.empty:
+        return player['key_mlbam'].values[0]
+    else:
+        raise ValueError(f'Player {first_name} {last_name} not found.')
+    
+def get_pitcher_data(first_name: str , last_name: str, start_dt: str, end_dt: str) -> 'pd.DataFrame':
+    '''Get the statcast data for a given pitcher and date range. Returns a Dataframe with statcast data.'''
+    player_id = get_player_id(first_name, last_name)
+    data = statcast_pitcher(start_dt, end_dt, player_id)
+    return data
 
 def main():
+    data = get_pitcher_data('shohei', 'ohtani', '2018-01-01', '2023-12-31')
+    print(data.head())
+    print(data.columns.tolist())
     response = requests.get("https://httpbin.org/get")
     print(response.status_code)
     print(response.json())
