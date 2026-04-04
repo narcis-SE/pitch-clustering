a957206c94c4514cda27d7033accbddbb207a7c4
cstreitmatter <cfs9582@gmail.com>
2026-04-04 00:57:40 +0000

    added detailed pitch data

--- main.py
+++ main.py
@@ -78,16 +78,37 @@ def main():
     # we could show 3, 5, 10 year evolutions where possible?
     # pitchers = ['justin verlander', 'max scherzer', 'chris sale', 'gerrit cole', 'corbin burnes', 'clayton kershaw', 'yu darvish', 'garrett crochet', 'mason miller']
     # keepCols = ["game_date", "player_name", "pitcher", "pitch_type", "pitch_name", "release_speed", "release_spin_rate", "pfx_x", "pfx_z", "release_pos_x", "release_pos_z", "release_extension", "spin_axis"]
-
+    # pitchers = [
+    #             "Logan Gilbert", "Seth Lugo", "Logan Webb", "Zack Wheeler", "Aaron Nola",
+    #             "Corbin Burnes", "Clayton Kershaw", "Tarik Skubal", "George Kirby", "Dylan Cease",
+    #             "Sean Manaea", "Cristopher Sánchez", "Brandon Pfaadt", "Kevin Gausman", "JP Sears",
+    #             "Bryce Miller", "Brady Singer", "Tyler Anderson", "Tanner Houck", "Bailey Ober",
+    #             "Garrett Crochet", "Yu Darvish", "mason miller", "Ryan Pepiot", "Chris Sale",
+    #             "Reid Detmers", "Tyler Glasnow", "Ranger Suárez", "Paul Skenes", "Yency Almonte",
+    #             "Taijuan Walker", "Gerrit Cole", "Triston McKenzie", "Walker Buehler", "Jacob deGrom",
+    #             "Justin Verlander", "Max Scherzer", "Jacob Bird", "Kyle Gibson", "Miles Mikolas"
+    #         ]
+    # keepCols = [
+    #             "pitch_type", "game_date", "release_speed", "release_pos_x", "release_pos_z",
+    #             "player_name", "pitcher", "events", "description", "spin_dir",
+    #             "zone", "des", "p_throws", "type", "game_year",
+    #             "release_spin_rate", "release_extension", "release_pos_y", "babip_value", "iso_value",
+    #             "launch_speed", "pitch_number", "pitch_name", "spin_axis", "hyper_speed",
+    #             "age_pit", "n_thruorder_pitcher", "pitcher_days_since_prev_game", "arm_angle"
+    #         ]
     # tempData = []
     # for pitcherName in pitchers:
     #     print(f'working on pitcher {pitcherName}')
     #     fullName = pitcherName.split(' ')
-    #     temp = get_pitcher_data(fullName[0], fullName[1], '2015-01-01', '2025-12-31').filter(keepCols).dropna()
-    #     tempData.append(temp)
-
+    #     try:
+    #         temp = get_pitcher_data(fullName[0], fullName[1], '2015-01-01', '2025-12-31').filter(keepCols)#.dropna()
+    #         tempData.append(temp)
+    #         # print(len(temp))
+    #     except ValueError as e:
+    #         print(e)
+    # print('data retrieval complete, concatenating and writing to csv')
     # data = pd.concat(tempData, ignore_index = True)
-    # data.to_csv('pitcher_data.csv', index = False)
+    # data.to_csv('pitcher_data_detailed.csv', index = False)
     # print(data.head())
     # print(data.shape[0])
 
